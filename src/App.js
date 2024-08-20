39e6816b022b69b931d5a91dd6b78f5f6db979d5
Bhavika Kothari <bhavika5@illinois.edu>
2024-08-19 19:29:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],  
+      searchfield: ''    
+
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState(() => {
+        return {monsters: users};
+      },
+      () => {
+        console.log(this.state);
+      }
+
+      ));
+  }
+
+  onsearchchange = (event) => {console.log(event.target.value);
+    const searchfield = event.target.value.toLocaleLowerCase();      
+
+    this.setState(() => {
+      return {searchfield};
+    });
+  };
+
+  render () {
+
+    const {monsters, searchfield} = this.state;
+    const {onsearchchange} = this;
+
+    const filteredmonsters= monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchfield);
+
+    });
+
+    return (
+      <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
+
+        <SearchBox className = 'monsters-search-box' onChangeHandler = {onsearchchange} placeholder = 'search monsters'/>
+       
+        <CardList monsters={filteredmonsters}/>
+       
+        
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
